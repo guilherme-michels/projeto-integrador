44cbf4041543471d6ae1222b576395fc75d35fe3
Guilherme <Michels>
2022-12-05 21:26:07 -0300

    remove importacoes

--- back/src/routes/routes.ts
+++ back/src/routes/routes.ts
@@ -4,19 +4,11 @@ import { personRouter } from './person';
 import { taskRouter } from './task';
 import { authRouter } from './auth';
 import { projectRouter } from './project';
-import { projectTasksRouter } from './projectTasks';
-import { projectUsersRouter } from './projectUsers';
-import { teamRouter } from './team';
-import { teamUsersRouter } from './teamUsers';
 
 const routes = express.Router();
 routes.use(personRouter);
 routes.use(taskRouter);
 routes.use(authRouter);
 routes.use(projectRouter);
-routes.use(projectTasksRouter);
-routes.use(teamRouter);
-routes.use(projectUsersRouter);
-routes.use(teamUsersRouter);
 
 export default routes;
